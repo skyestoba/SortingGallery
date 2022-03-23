612158913522118ee71071e4b7b43c569c2d63e5
mandigo.skyler <mandigo.skyler@vashonsd.org>
2022-03-23 10:39:50 -0700

    Finished merge

--- src/main/java/org/example/Sort.java
+++ src/main/java/org/example/Sort.java
@@ -8,7 +8,11 @@ public class Sort {
      * @param nums
      */
     public static void sortTwo(int[] nums) {
-
+        if(nums[0]>nums[1]){
+            int hold = nums[0];
+            nums[0]=nums[1];
+            nums[1]=hold;
+        }
     }
 
     /**
@@ -19,6 +23,45 @@ public class Sort {
      * @param array2 - A sorted list of numbers that is the size of half the length of the inputArray
      */
     public static void merge(int[] inputArray, int[] array1, int [] array2) {
+//        for(int i = 0; i<array1.length+array2.length-1; i++){
+//            if(array1[i]<array2[i]){
+//                inputArray[i]=array1[i];
+//                inputArray[i+1]=array2[i];
+//            }
+//            inputArray[i+1]=array1[i];
+//            inputArray[i]=array2[i];
+//        }
 
+        int i=0;
+        int j=0;
+        int k=0;
+
+        //while there is still room in LH and RH lists
+        while(i<array1.length && j<array2.length){
+            if(array1[i]<array2[j]){
+                inputArray[k]=array1[i];
+                i++;
+                k++;
+            }
+            if(array1[i]>array2[j]){
+                inputArray[k]=array2[j];
+                j++;
+                k++;
+            }
+        }
+
+        while(array1.length>array2.length && k<inputArray.length){
+            inputArray[k]=array1[i];
+            i++;
+            k++;
+        }
+        while(array1.length<array2.length && k<inputArray.length){
+            inputArray[k]=array2[j];
+            j++;
+            k++;
+        }
+            //compare, put the lowest in te long list
+            //push the index for the winner
+        //clean out lists into the end of big list
     }
 }
